ad815377e74117a85c173713f93f9e0240f8a90b
Léo Teissier <leo.teissier@numericable.fr>
2023-06-20 17:07:03 +0200

    update periodic table

--- src/views/PeriodicTableView.vue
+++ src/views/PeriodicTableView.vue
@@ -1,5 +1,6 @@
 <script>
 import { useElementStore } from '@/stores/index.js';
+import { gsap } from "gsap";
 
 export default {
   name: 'PeriodicTableView',
@@ -7,6 +8,19 @@ export default {
     return {
       grid: [],
       selectedElement: null,
+      selectedCategory: null,
+      categories: [
+        'alkali metal',
+        'alkaline earth metal',
+        'actinide',
+        'lanthanide',
+        'transition metal',
+        'post-transition metal',
+        'metalloid',
+        'nonmetal',
+        'halogen',
+        'noble gas'
+      ],
     }
   },
   computed: {
@@ -25,6 +39,7 @@ export default {
     const elementStore = useElementStore();
     await elementStore.fetchElements();
     this.createGrid(elementStore.elements);
+    window.addEventListener('click', this.handleOutsideClick);
   },
   methods: {
     createGrid(elements) {
@@ -40,8 +55,6 @@ export default {
           };
         }
       });
-
-      console.log('Grid:', this.grid);
     },
     getElementPosition(atomicNumber) {
       let row = 0;
@@ -86,7 +99,11 @@ export default {
       return {row, column};
     },
     selectElement(element) {
-      this.selectedElement = element; // Sélectionner un élément
+      this.selectedElement = element;
+      this.selectedCategory = element.category;
+      this.$nextTick(() => {
+        this.animateElectrons(this.electronConfig);
+      });
     },
     getColorByCategory(category) {
       switch (category) {
@@ -107,7 +124,7 @@ export default {
     parseElectronConfig(config) {
       let electronLayers = config.split(' ');
       let electronCount = {};
-      console.log(electronLayers);
+
       electronLayers.forEach((layer, index) => {
         let matchResult = layer.match(/(\d)[spdfghijklmnoqrtuvwxyz](\d+)/);
         if (matchResult) {
@@ -126,84 +143,153 @@ export default {
         }
       });
       return electronCount;
-    }
-  }
+    },
+    animateElectrons(electronConfig) {
+      Object.keys(electronConfig).forEach(layer => {
+        const electronLayer = this.$refs[`electronLayer-${layer}`][0];
+
+        gsap.to(electronLayer, {
+          rotation: "+=360", // Add 360 degrees to the current rotation
+          duration: 10, // Animation duration in seconds
+          repeat: -1, // Repeat indefinitely
+          ease: "linear", // Uniform movement
+        });
+      });
+    },
+    RoundNumber(number) {
+      return Number(number.toFixed(3));
+    },
+    handleOutsideClick(event) {
+      // Vérifiez si l'événement provient d'un élément du tableau
+      if (!event.target.closest('.elements')) {
+        this.selectedCategory = null;
+      }
+    },
+  },
+  beforeDestroy() {
+    // Supprimez l'écouteur d'événement lorsque le composant est détruit pour éviter les fuites de mémoire
+    window.removeEventListener('click', this.handleOutsideClick);
+  },
 }
 </script>
 
 <template>
   <h1 class="title">Periodic Table</h1>
   <div v-if="loading">
-    Chargement en cours...
+    Loading...
   </div>
   <div v-else-if="!grid.length">
-    Pas de données.
+    No data available.
   </div>
   <div v-else-if="flatGrid.length" class="tableau">
     <div v-for="(element, i) in flatGrid.filter(e => e !== null)"
-       :key="i" @click.prevent="selectElement(element)"
-       class="element"
-       :style="{gridColumn: `${element.column+1} / span 1`,gridRow: `${element.row+1} / span 1`, backgroundColor:`${getColorByCategory(element.category)}`}"
+         :key="i" @click.prevent="selectElement(element)"
+         class="elements"
+         :class="{ selectedElement: selectedElement && selectedElement.number === element.number, notSelectedClass: selectedCategory && selectedCategory !== element.category, selectedClass: selectedCategory === element.category }"
+         :style="{ gridColumn: `${element.column + 1} / span 1`, gridRow: `${element.row + 1} / span 1`, backgroundColor: `${getColorByCategory(element.category)}` }"
     >
       <p class="number">{{ element.number }}</p>
       <h3 class="symbol">{{ element.symbol }}</h3>
       <p class="name">{{ element.name }}</p>
     </div>
+
     <div class="information" v-if="selectedElement">
       <div class="info1">
         <h1><router-link :to="`/element/${selectedElement.symbol}`">{{ selectedElement.number }} - {{ selectedElement.name }}</router-link></h1>
-        <h2 :style="{color:`${getColorByCategory(selectedElement.category)}`}">{{ selectedElement.category }}</h2>
+        <h2 :style="{
+          color: `${getColorByCategory(selectedElement.category)}`, textTransform: 'capitalize',}">{{ selectedElement.category }}</h2>
       </div>
       <div class="info2">
-        <p>phase</p>
+        <h2>Phase</h2>
+        <img v-if="selectedElement.phase === 'Solid'" class="element-etat" src="/solid.svg" alt="icon solid">
+        <img v-if="selectedElement.phase === 'Liquid'" class="element-etat" src="/liquid.svg" alt="icon liquid">
+        <img v-if="selectedElement.phase === 'Gas'" class="element-etat" src="/gas.svg" alt="icon gas">
       </div>
       <div class="info3">
-        <p>Atomic Mass</p>
-        <h2>{{ selectedElement.atomic_mass }}</h2>
+        <h2>Atomic Mass</h2>
+        <h1>{{ RoundNumber(selectedElement.atomic_mass)}}</h1>
       </div>
       <div class="info4">
-        <p>Density</p>
-        <h2>{{ selectedElement.density }}</h2>
+        <h2>Density</h2>
+        <h1>{{  RoundNumber(selectedElement.density) }}</h1>
       </div>
 
       <div class="atom">
-        <div class="nucleon" :style="{backgroundColor:`${getColorByCategory(selectedElement.category)}`}">
-          <h2>{{selectedElement.symbol}}</h2>
+        <div class="nucleon" :style="{ backgroundColor: `${getColorByCategory(selectedElement.category)}` }">
+          <h2 style="z-index: 999"><router-link :to="`/element/${selectedElement.symbol}`">{{ selectedElement.symbol }}</router-link></h2>
         </div>
-        <div v-for="(electronCount, layer) in electronConfig" :key="layer" class="electron-layer" :style="{width: `${70 + layer * 20}px`, height: `${70 + layer * 20}px`, border:`1px solid ${getColorByCategory(selectedElement.category)}`}">
-          <div v-for="(electron, index) in electronCount" :key="index" class="electron" :style="{backgroundColor:`${getColorByCategory(selectedElement.category)}`, transform: `rotate(${360 * index / electronCount.length}deg) translate(${30 + layer * 10}px)`}"/>
+        <div v-for="(electronCount, layer) in electronConfig"
+             :key="layer"
+             class="electron-layer"
+             :style="{ width: `${70 + layer * 20}px`, height: `${70 + layer * 20}px`, border: `1px solid ${getColorByCategory(selectedElement.category)}` }"
+             :ref="`electronLayer-${layer}`">
+          <div v-for="(electron, index) in electronCount"
+               :key="index"
+               class="electron"
+               :style="{ backgroundColor: `${getColorByCategory(selectedElement.category)}`, transform: `rotate(${360 * index / electronCount.length}deg) translate(${30 + layer * 10}px)` }">
+          </div>
         </div>
       </div>
-
+    </div>
+    <div :class="{'tab-57-71': true, 'selectedClass': selectedCategory === 'lanthanide', 'notSelectedClass': selectedCategory && selectedCategory !== 'lanthanide'}">
+      <p class="number">57-71</p>
+      <p class="name">Lanthanides</p>
+    </div>
+    <div :class="{'tab-89-103': true, 'selectedClass': selectedCategory === 'actinide', 'notSelectedClass': selectedCategory && selectedCategory !== 'actinide'}">
+      <p class="number">89-103</p>
+      <p class="name">Actinides</p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "src/assets/atom.scss";
+$width-height-cell: 85px;
 .title{
-  text-align: center;
-  margin: 20px 0;
-  font-size: 2rem;
-  color: #919EAB;
+  grid-column: 1/2;
+  grid-row: 1/2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5rem;
 }
 
 .tableau {
+  grid-column: 1/2;
+  grid-row: 2/3;
   display: grid;
   grid-template-rows: repeat(7, 1fr) 25px repeat(2, 1fr);
   grid-template-columns: repeat(18, 1fr);
   gap: 1px;
+  width: fit-content;
+  height: fit-content;
+  margin: auto;
 
-  .element{
-    width: 80px;
-    height: 80px;
+  .selectedElement {
+    transition: 0.2s ease-in-out ;
+    color: white !important;
+  }
+
+  .selectedClass {
+    transition: 0.2s ease-in-out ;
+    filter: drop-shadow(2px 4px 6px black);
+    opacity: 1 !important;
+  }
+
+  .notSelectedClass {
+    transition: 0.2s ease-in-out ;
+    opacity: 0.5;
+  }
+
+  .elements{
+    width: $width-height-cell;
+    height:  $width-height-cell;
     display: grid;
     grid-template-rows: 20px 1fr 20px;
-    grid-template-columns: 20px 1fr;
+    grid-template-columns: 25px 1fr;
     text-decoration: none;
     color: #919EAB;
     cursor: pointer;
-    z-index: 2;
     &:hover{
       filter: drop-shadow(2px 4px 6px black);
     }
@@ -219,19 +305,20 @@ export default {
     .symbol {
       grid-column: 1/3;
       grid-row: 2/3;
+      display: flex;
+      justify-content: center;
+      align-items: center;
       font-size: 1.7rem;
       font-weight: bold;
     }
 
-    .name {
-      grid-column: 1/3;
-      grid-row: 3/4;
-      font-size: 0.8rem;
-      margin: 0 4px;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
+  }
+  .name {
+    grid-column: 1/3;
+    grid-row: 3/4;
+    font-size: 0.8rem;
+    margin: 0 4px;
+    text-align: center;
   }
   .information{
     grid-column: 3/13;
@@ -260,6 +347,10 @@ export default {
       flex-direction: column;
       justify-content: space-evenly;
       align-items: center;
+      .element-etat{
+        width: 50px;
+        height: 50px;
+      }
     }
 
     .info3{
@@ -280,6 +371,46 @@ export default {
       align-items: center;
     }
   }
-}
+  .tab-57-71{
+    grid-column: 3/4;
+    grid-row: 6/7;
+    width: $width-height-cell;
+    height: $width-height-cell;
+    background-color: #453F60;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
+  .tab-89-103{
+    grid-column: 3/4;
+    grid-row: 7/8;
+    width: $width-height-cell;
+    height: $width-height-cell;
+    background-color: #3B2E48;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    font-weight: bold;
+  }
+  .categories{
+    grid-column: 1/4;
+    grid-row: 9/11;
+    padding: 0 10px 0 0;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: 1fr;
+    div{
+      display: flex;
+      font-size: 16px;
+      writing-mode: vertical-rl;
+      cursor: pointer;
+    }
 
+  }
+}
 </style>
